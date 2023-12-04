dd02f2367b0ff92e7c6209df00151753d5423509
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-12-04 14:29:48 +0300

    DepartmentRepository done

---
+++ src/main/java/com/techg/department/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.techg.department.repository;
+
+import com.techg.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+}
